eba1ea081a39e6f75cceea12727264368c5d07b3
Sehrish Ijaz <sia.ansari11@gmail.com>
2023-01-31 21:53:25 +0000

    Added network module again

--- network.tf
+++ network.tf
@@ -4,7 +4,9 @@ resource "azurerm_resource_group" "example" {
 }
 
 module "network" {
-  source              = "Azure/network/azurerm"
+  source  = "app.terraform.io/ijaz/network/azurerm"
+  version = "3.5.0"
+  # insert required variables here
   resource_group_name = azurerm_resource_group.example.name
   address_spaces      = ["10.0.0.0/16", "10.2.0.0/16"]
   subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
