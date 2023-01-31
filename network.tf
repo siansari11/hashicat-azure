ab3114f0ab63d62f484fb843a7087abae006b601
Sehrish Ijaz <sia.ansari11@gmail.com>
2023-01-31 22:00:40 +0000

    changed azurerm name

--- network.tf
+++ network.tf
@@ -1,4 +1,4 @@
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "siansari" {
   name     = "${var.prefix}-workshop"
   location = var.location
 }
@@ -7,7 +7,7 @@ module "network" {
   source  = "app.terraform.io/ijaz/network/azurerm"
   version = "3.5.0"
   # insert required variables here
-  resource_group_name = azurerm_resource_group.example.name
+  resource_group_name = azurerm_resource_group.siansari.name
   address_spaces      = ["10.0.0.0/16", "10.2.0.0/16"]
   subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   subnet_names        = ["subnet1", "subnet2", "subnet3"]
@@ -23,5 +23,5 @@ module "network" {
     costcenter  = "it"
   }
 
-  depends_on = [azurerm_resource_group.example]
+  depends_on = [azurerm_resource_group.siansari]
 }
